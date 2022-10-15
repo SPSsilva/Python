149e8eb140e42e1b38f26711124692d99720b84a
Saulo Pinheiro <103376962+SPSsilva@users.noreply.github.com>
2022-10-14 22:02:59 -0300

    Up 45

--- Mundo 2/Ex. 45.py
+++ Mundo 2/Ex. 45.py
@@ -1,1 +1,31 @@
-S =random(1 , 2, 3)
+import random
+S = int(input('''Qual a sua escolha?
+    [1] PEDRA
+    [2] PAPEL
+    [3] TESOURA
+Qual é a sua jogada? '''))
+P = random.randint(1, 3)
+if S == P:
+    print('Deu empate, ambos escolheram ', end='')
+    if S == 1 == P:
+        print('Pedra.')
+    elif S == 2 == P:
+        print('PAPEL.')
+    elif S == 3 == P:
+        print('TESOURA.')
+elif S != P and P < 4 and S < 4:
+    if S == 1 and P == 2:
+        print('Papel enrola Pedra, você perdeu!')
+    elif S == 1 and P == 3:
+        print('Pedra quebra Tesoura, você ganhou!')
+    elif S == 2 and P == 1:
+        print('Papel enrola Pedra, você ganhou!')
+    elif S == 2 and P == 3:
+        print('Tesoura corta Papel, você perdeu!')
+    elif S == 3 and P == 1:
+        print('Pedra quebra Tesoura, você perdeu!')
+    elif S == 3 and P == 2:
+        print('Tesoura corta Papel, você ganhou!')
+else:
+    print('Escolha um número de 1 a 3.')
+print(P)
