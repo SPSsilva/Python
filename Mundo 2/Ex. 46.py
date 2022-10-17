3e7709f34fe6a5b29b9b75f5f736e5df9b1613c3
Saulo Pinheiro <103376962+SPSsilva@users.noreply.github.com>
2022-10-16 22:16:37 -0300

    16/10

---
+++ Mundo 2/Ex. 46.py
@@ -0,0 +1,8 @@
+import time
+import emoji
+for C in range(10, 0, -1):
+    print(C)
+    time.sleep(1)
+print('Carregando...')
+time.sleep(2)
+print(emoji.emojize('Parabéns!!!:fireworks::fireworks:'))
