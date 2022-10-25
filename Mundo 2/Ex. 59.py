0a82859f62622311bc37924ee863f01906e08212
Saulo Pinheiro <103376962+SPSsilva@users.noreply.github.com>
2022-10-24 23:29:22 -0300

    24/10 1.1

--- Mundo 2/Ex. 59.py
+++ Mundo 2/Ex. 59.py
@@ -1,9 +1,35 @@
-J = 5
-while J == 5:
-    S = int(input('Digite um número: '))
-    N = int(input('Digite mais um número: '))
-    print('''[1] Somar
+import time
+S = int(input('Digite um número: '))
+N = int(input('Digite mais um número: '))
+P = 10
+while P != 0:
+    print('''       MENU DE OPÇÕES:
+    [1] Somar
     [2] Multiplicar
     [3] Maior
     [4] Novos números
     [5] Sair o programa''')
+    P = int(input('Digite um número correspondente as opções: '))
+    if P == 1:
+        print(f'{S} + {N} = {S+N}')
+        time.sleep(5)
+    elif P == 2:
+        print(f'{S} * {N} = {S*N}')
+        time.sleep(5)
+    elif P == 3:
+        if S > N:
+            print('{} é maior que {}!'.format(S, N))
+        elif S == N:
+            print('Os número são iguais, portanto não há maior.')
+        else:
+            print('{} é maior que {}!'.format(N, S))
+        time.sleep(5)
+    elif P == 4:
+        S = int(input('Digite um número: '))
+        N = int(input('Digite mais um número: '))
+    elif P == 5:
+        P = 0
+    else:
+        P = 10
+print('Saindo...')
+time.sleep(2)
