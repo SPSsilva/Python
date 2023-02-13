8110faf2741b4335ca12bccdd999c3ebf18fcd07
Saulo Pinheiro <103376962+SPSsilva@users.noreply.github.com>
2023-02-13 15:46:25 -0300

    13/02/23

--- Mundo 3/Ex. 79.py
+++ Mundo 3/Ex. 79.py
@@ -1,1 +1,14 @@
-P
+lista = []
+while True:
+    P = int(input('Digite um número: '))
+    if P not in lista:
+        lista.append(P)
+    else:
+        print('Este número que você digitou já foi adicionado anteriormente. Por favor tente novamente!')
+    S = str(input('Você quer continuar? [S/N]'))
+    if S in 'nN':
+        break
+lista.sort()
+print('-=-'*25)
+print('A seguir a lista dos números unicos e123 digitados em ordem crescente.')
+print(lista)
